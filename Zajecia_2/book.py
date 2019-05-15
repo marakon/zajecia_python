8b9b1231fa1c9ed371853d5fd8bfa69ce4f6b799
heheszek <mateuszosinski96@gmail.com>
2019-05-15 19:27:18 +0200

    zmiany

--- Zajecia_2/book.py
+++ Zajecia_2/book.py
@@ -1,9 +1,16 @@
-book = input("What is the title of the book?: ")
-author = input("What is the surname of author?: ")
-sites = input("How many sites it has?: ")
-book2 = book.isalpha()
-author2 = author.isalpha()
-book2 = book.title()
-author2 = author.title()
-book1 = book2 + " " + author2 + " " + sites
-print(book1, str(len(book1)))
+
+book = str(input("What is the title of the book?: "))
+author = str(input("What is the surname of author?: "))
+sites = int(input("How many sites it has?: "))
+def book_func(str book, str author, int sites):
+    book2 = book.isalpha()
+    author2 = author.isalpha()
+    book2 = book.capitalize()
+    author2 = author.capitalize()
+    book1 = book2 + " " + author2 + " " + str(sites)
+    print(book1, str(len(book1)))
+
+def test_book_func():
+    assert book_func()
+
+book_func(book, author, sites)
