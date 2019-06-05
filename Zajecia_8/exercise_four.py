3a511ec9f8f9dcf14513ee5ba29dcd2f61698a54
heheszek <mateuszosinski96@gmail.com>
2019-06-05 18:14:57 +0200

    next exercise

---
+++ Zajecia_8/exercise_four.py
@@ -0,0 +1,29 @@
+""" Oblicz średnią arymetyczną z kilku liczb.
+    Liczby będą podane przez użytkownika po przecinku.
+    Napisz funkcję, która przyjmie wartości i wyświetli średnią.
+    Program powinen być odporny na błędy użytkownika."""
+try:
+    n = int(input("How many numbers?: "))
+except ValueError:
+    print('Enter int!')
+    exit()
+if n <= 0:
+    raise Exception('You have to enter at least one element!')
+
+sumer = 0
+numbers = []
+for number in range(n):
+    value = int(input("Enter value: "))
+    numbers.append(value)
+    sumer += value
+
+if sumer == 0:
+    raise Exception('Moron')
+
+for number in numbers:
+    print(number)
+
+print(sumer)
+
+avg = sumer / n
+print(avg)
