7f996ec40360621cec6d33845b71079399cc043a
heheszek <mateuszosinski96@gmail.com>
2019-06-05 17:31:15 +0200

    zadanko 2

--- Zajecia_8/exercise_two.py
+++ Zajecia_8/exercise_two.py
@@ -1,3 +1,24 @@
 """ Utwórz dowolną krotkę zawierającą kilka wartości np. 10.
     Pozwól użytkownikowi podać dowolny index oraz wartość.
     Spróbuj w krotce podmienić wartość na zadanym indeksie. Obsłuż błąd"""
+
+tuple_examples = ('element one', 2, '3', 'four')
+print(type(tuple_examples))
+n = 0
+for element in tuple_examples:
+    print(element)
+    n += 1
+
+print('This tuple has: {0} elements'.format(n))
+print('Let\'s add an element to index {0}!'. format(n))
+
+try:
+    tuple_examples[n] = 'next element'
+except TypeError:
+    print("You can not change a tuple... Moron...")
+
+print("Let\'s change an element!")
+try:
+    tuple_examples[0] = 1
+except TypeError:
+    print("You can not change a tuple as I have told you... Moron x2...")
