ef48d9496c0190958a0581343c35a098455ac641
heheszek <mateuszosinski96@gmail.com>
2019-06-05 17:22:22 +0200

    kolejne zaczynamy

--- Zajecia_8/exercise_one.py
+++ Zajecia_8/exercise_one.py
@@ -1,7 +1,3 @@
-""" Stwórz listę składającą się z kilku elementów różnego typu.
-    W pętli spróbuj wykonać dzielenie 10 przez wartość z listy.
-    Złap wyjątki jakie mogą się przy tej okazji wydarzyć."""
-
 examples = [0.0, 10, 'one', ('cos', 5), {'klucz': 'wartosc'}]
 
 for element in examples:
---
+++ Zajecia_8/exercise_two.py
@@ -0,0 +1,3 @@
+""" Utwórz dowolną krotkę zawierającą kilka wartości np. 10.
+    Pozwól użytkownikowi podać dowolny index oraz wartość.
+    Spróbuj w krotce podmienić wartość na zadanym indeksie. Obsłuż błąd"""
